365210c4ee61be1c86e88553da5398e7a3cf5347
Lawrence <lawrencefej@gmail.com>
2019-02-24 13:44:23 -0500

    Eager Loaded Asset type to the asset search method

--- LMSRepository/DataAccess/LibraryAssetRepository.cs
+++ LMSRepository/DataAccess/LibraryAssetRepository.cs
@@ -107,13 +107,13 @@ namespace LMSLibrary.DataAccess
         public async Task<IEnumerable<LibraryAsset>> SearchLibraryAsset(string searchString)
         {
             var assets = from asset in _context.LibraryAssets
-                    .Include(s => s.Author)
+                        .Include(s => s.Author)
+                        .Include(s => s.AssetType)
                          select asset;
 
             if (!string.IsNullOrEmpty(searchString))
             {
                 assets = assets
-                    .Include(s => s.Author)
                     .Where(s => s.Title.Contains(searchString)
                     || s.Author.LastName.Contains(searchString)
                     || s.Author.FirstName.Contains(searchString));
