7f2831927f15a56edfa2369fd81602d25b5abed7
Lawrence <lawrencefej@gmail.com>
2019-03-04 21:35:17 -0500

    Added Author Functionality

--- LMSService/Service/AuthorService.cs
+++ LMSService/Service/AuthorService.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using LMSRepository.Dto;
 using LMSRepository.Interfaces;
+using LMSRepository.Interfaces.Models;
+using LMSService.Exceptions;
 using LMSService.Interfaces;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -13,27 +16,67 @@ namespace LMSService.Service
         private readonly IAuthorRepository _authorRepository;
         private readonly IMapper _mapper;
         private readonly ILibraryRepository _libraryRepository;
+        private readonly ILogger<AuthorService> _logger;
 
-        public AuthorService(IAuthorRepository authorRepository, IMapper mapper, ILibraryRepository libraryRepository)
+        public AuthorService(IAuthorRepository authorRepository, IMapper mapper, ILibraryRepository libraryRepository, ILogger<AuthorService> logger)
         {
             _authorRepository = authorRepository;
             _mapper = mapper;
             _libraryRepository = libraryRepository;
+            _logger = logger;
         }
 
-        public Task AddAuthor(AuthorDto authorDto)
+        public async Task<AuthorDto> AddAuthor(AuthorDto authorDto)
         {
-            throw new NotImplementedException();
+            //TODO add unique logic
+            if (authorDto == null)
+            {
+                throw new NoValuesFoundException("The author is null");
+            }
+
+            var author = _mapper.Map<Author>(authorDto);
+
+            _libraryRepository.Add(author);
+
+            if (!await _libraryRepository.SaveAll())
+            {
+                throw new Exception($"Creating {author.FullName} failed on save");
+            }
+
+            var authorToReturn = _mapper.Map<AuthorDto>(author);
+
+            _logger.LogInformation($"added {author.FullName}");
+
+            return authorToReturn;
         }
 
-        public Task DeleteAuthor(int authorId)
+        public async Task DeleteAuthor(int authorId)
         {
-            throw new NotImplementedException();
+            //TODO add logs
+            var author = await _authorRepository.GetAuthor(authorId);
+
+            _libraryRepository.Delete(author);
+
+            if (await _libraryRepository.SaveAll())
+            {
+                return;
+            }
+
+            throw new Exception($"Deleting {author.FullName} failed on save");
         }
 
-        public Task EditAuthor(AuthorDto authorDto)
+        public async Task EditAuthor(AuthorDto authorDto)
         {
-            throw new NotImplementedException();
+            var author = await _authorRepository.GetAuthor(authorDto.Id);
+
+            _mapper.Map(authorDto, author);
+
+            if (!await _libraryRepository.SaveAll())
+            {
+                throw new Exception($"Updating {author.FullName} failed on save");
+            }
+
+            return;
         }
 
         public async Task<AuthorDto> GetAuthor(int authorId)
---
+++ LibraryManagementSystem/Controllers/AuthorController.cs
@@ -0,0 +1,66 @@
+﻿using LMSRepository.Dto;
+using LMSService.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Policy = "RequireLibrarianRole")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAuthors()
+        {
+            var authors = await _authorService.GetAuthors();
+
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}", Name = "GetAuthor")]
+        public async Task<IActionResult> GetAuthor(int authorId)
+        {
+            var author = await _authorService.GetAuthor(authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditAuthor(AuthorDto authorDto)
+        {
+            await _authorService.EditAuthor(authorDto);
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAuthor(AuthorDto authorDto)
+        {
+            var author = await _authorService.AddAuthor(authorDto);
+
+            return CreatedAtRoute("GetAuthor", new { authorId = author.Id }, author);
+        }
+
+        [HttpDelete("{authorId}")]
+        public async Task<IActionResult> DeleteAuthor(int authorId)
+        {
+            await _authorService.DeleteAuthor(authorId);
+
+            return NoContent();
+        }
+    }
+}
