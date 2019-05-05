c062aaba2c462086cfaef3790ccf22c95294f29c
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-05-04 21:00:22 -0400

    Fixed conflict

---
+++ LMSRepository/Dto/AssetPhotoDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMSRepository.Dto
+{
+    public class AssetPhotoDto
+    {
+        public string Url { get; set; }
+
+        [Required]
+        public IFormFile File { get; set; }
+
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
+        public int LibraryAssetId { get; set; }
+
+        public AssetPhotoDto()
+        {
+            DateAdded = DateTime.Now;
+        }
+    }
+}
