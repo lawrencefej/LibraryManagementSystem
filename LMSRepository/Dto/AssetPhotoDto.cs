030f508cc0b8fb14a2e364d31b1bf83591260768
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-05-04 20:34:05 -0400

    Made Id's and File required

--- LMSRepository/Dto/AssetPhotoDto.cs
+++ LMSRepository/Dto/AssetPhotoDto.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LMSRepository.Dto
 {
     public class AssetPhotoDto
     {
         public string Url { get; set; }
+
+        [Required]
         public IFormFile File { get; set; }
+
         public string Description { get; set; }
         public DateTime DateAdded { get; }
         public string PublicId { get; set; }
+
+        [Required]
         public int LibraryAssetId { get; set; }
 
         public AssetPhotoDto()
