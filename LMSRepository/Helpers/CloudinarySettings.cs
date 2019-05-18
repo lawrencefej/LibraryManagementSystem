96cdd5fe7ba5fd72b8a479a58074856a858fddd6
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-05-18 13:50:07 -0400

    added stronggly typed configurations

--- LMSRepository/Helpers/CloudinarySettings.cs
+++ LMSRepository/Helpers/CloudinarySettings.cs
@@ -1,9 +1,16 @@
-﻿namespace LMSRepository.Interfaces.Helpers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMSRepository.Interfaces.Helpers
 {
     public class CloudinarySettings
     {
+        [Required]
         public string CloudName { get; set; }
+
+        [Required]
         public string ApiKey { get; set; }
+
+        [Required]
         public string ApiSecret { get; set; }
     }
 }
---
+++ LibraryManagementSystem/Helpers/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Helpers
+{
+    public class AppSettings
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 16, ErrorMessage = "Token must be 16 characters or more")]
+        public string Token { get; set; }
+
+        [Required]
+        public string ConnectionString { get; set; }
+    }
+}
