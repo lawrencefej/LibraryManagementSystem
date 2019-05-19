352154d0a995166d304285516f1477b105d50f48
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-05-18 21:24:50 -0400

    Added strongly typed appsettings

---
+++ LibraryManagementSystem/Helpers/SettingValidationStartupFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryManagementSystem.Helpers
+{
+    public class SettingValidationStartupFilter : IStartupFilter
+    {
+        private readonly IEnumerable<IValidatable> _validatableObjects;
+
+        public SettingValidationStartupFilter(IEnumerable<IValidatable> validatableObjects)
+        {
+            _validatableObjects = validatableObjects;
+        }
+
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            foreach (var validatableObject in _validatableObjects)
+            {
+                validatableObject.Validate();
+            }
+
+            return next;
+        }
+    }
+}
