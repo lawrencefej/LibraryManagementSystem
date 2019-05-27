50d68eccba05223e676412231cf236bca05feac6
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-05-27 14:18:34 -0400

    Added Paid fees simulation

---
+++ LMSService/Service/PaymentService.cs
@@ -0,0 +1,45 @@
+﻿using LMSRepository.Interfaces;
+using LMSService.Exceptions;
+using LMSService.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace LMSService.Service
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly ILibraryCardRepository _libraryCardRepository;
+        private readonly ILogger<PaymentService> _logger;
+        private readonly ILibraryRepository _libraryRepository;
+
+        public PaymentService(ILibraryCardRepository libraryCardRepository, ILogger<PaymentService> logger,
+                                ILibraryRepository libraryRepository)
+        {
+            _libraryCardRepository = libraryCardRepository;
+            _logger = logger;
+            _libraryRepository = libraryRepository;
+        }
+
+        public async Task PayFees(int libraryCardID)
+        {
+            var card = await _libraryCardRepository.GetCard(libraryCardID);
+
+            if (card == null)
+            {
+                _logger.LogWarning($"Library {libraryCardID} does not exist");
+                throw new NoValuesFoundException($"LibraryCard {libraryCardID} was not found");
+            }
+
+            card.Fees = 0;
+
+            if (await _libraryRepository.SaveAll())
+            {
+                _logger.LogInformation($"Fees were paid for Library Card {libraryCardID}");
+                return;
+            }
+
+            throw new Exception($"Unsuccessful payment for card {libraryCardID} failed on save");
+        }
+    }
+}
---
+++ LibraryManagementSystem/Controllers/FeeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using LibraryManagementSystem.API.Helpers;
+using LMSService.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Authorize(Policy = Role.RequireLibrarianRole)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeeController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public FeeController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost("{librayCardID}")]
+        public async Task<IActionResult> Post(int librayCardID)
+        {
+            await _paymentService.PayFees(librayCardID);
+
+            return Ok();
+        }
+    }
+}
