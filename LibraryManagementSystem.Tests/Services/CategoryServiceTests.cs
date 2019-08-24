a20139bd6e9f72025dac23b8c25ca6a06ccf800a
Lawrence Fejokwu <lawrencefej@gmail.com>
2019-08-24 16:36:55 -0400

    Added CategoryService tests

---
+++ LibraryManagementSystem.Tests/Services/CategoryServiceTests.cs
@@ -0,0 +1,103 @@
+﻿using LMSRepository.Models;
+using LMSService.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LibraryManagementSystem.Tests.Services
+{
+    public class CategoryServiceTests
+    {
+        private readonly TestDataContextFactory _factory;
+
+        public CategoryServiceTests()
+        {
+            _factory = new TestDataContextFactory();
+        }
+
+        private static Category GetCategory()
+        {
+            return new Category { Id = 4, Name = "Test4" };
+        }
+
+        [Fact]
+        public async Task AddCategory_ValidCategory_ShouldAddNewCategory()
+        {
+            using (var context = _factory.UseInMemory())
+            {
+                // Arrange
+                var service = new CategoryService(context);
+                var category = GetCategory();
+
+                // Act
+                var actual = await service.AddCategory(category);
+                var expected = category;
+
+                // Assert
+                Assert.NotNull(actual);
+                Assert.Equal(expected.Id, actual.Id);
+                Assert.Equal(expected.Name, actual.Name);
+            }
+        }
+
+        [Fact]
+        public async Task DeleteCategory_ExistingCategory_ShouldDeleteCategory()
+        {
+            using (var context = _factory.UseInMemory())
+            {
+                // Arrange
+                var service = new CategoryService(context);
+                var category = GetCategory();
+                context.Add(category);
+                context.SaveChanges();
+                category = context.Category.FirstOrDefault(x => x.Id == category.Id);
+
+                // Act
+                await service.DeleteCategory(category);
+                var expected = context.Category.FirstOrDefault(x => x.Id == category.Id);
+
+                // Assert
+                Assert.Null(expected);
+            }
+        }
+
+        [Fact]
+        public async void GetCategory_ListCategory_ReturnsCategoryList()
+        {
+            using (var context = _factory.UseInMemory())
+            {
+                //Act
+                var service = new CategoryService(context);
+                var actual = await service.GetCategories();
+                var expected = context.Category.Count();
+
+                //Assert
+                Assert.Equal(actual.Count(), expected);
+            }
+        }
+
+        [Fact]
+        public async void GetCategoryById_ExistingCategory_ReturnCategory()
+        {
+            using (var context = _factory.UseInMemory())
+            {
+                //Arrange
+                var category = GetCategory();
+                context.Add(category);
+                context.SaveChanges();
+
+                //Act
+                var service = new CategoryService(context);
+                var actual = await service.GetCategory(category.Id);
+                var expected = category;
+
+                //Assert
+                Assert.Equal(expected.Id, actual.Id);
+                Assert.Equal(expected.Name, actual.Name);
+            }
+        }
+    }
+}
