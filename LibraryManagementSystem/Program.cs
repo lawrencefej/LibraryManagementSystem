4604692ec6494d2f121c26d23d6802d2ac50da06
Lawrence <lawrencefej@gmail.com>
2019-10-17 22:35:12 -0400

    angular fixes

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Logging;
 
 namespace LibraryManagementSystem.API
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
